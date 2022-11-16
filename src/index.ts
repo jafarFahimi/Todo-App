55b31ebbe2a74811a6909b7731cfc42422a80fbe
jafar <jafar.fahimi12@gmail.com>
2022-11-16 10:56:39 +0430

    addListItem func completed & querySelectors refined

--- src/index.ts
+++ src/index.ts
@@ -6,25 +6,37 @@ type taskType = {
   createdAt: Date;
 };
 
-const list = document.querySelector<HTMLUListElement>('list'); // .getElementById don't has this generic func <>
+const list = document.querySelector<HTMLUListElement>('#list'); // .getElementById don't has this generic func <>
 const form = document.getElementById('new-task-form') as HTMLFormElement | null;
-const input = document.querySelector<HTMLInputElement>('new-task-title');
-
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+let tasks: taskType[] = [];
 form?.addEventListener('submit', (e) => {
   e.preventDefault();
   if (input?.value == '' || input?.value == null) return;
   // input is no longer null from now on.
-  const newTask : taskType = {
+  const newTask: taskType = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date(),
   };
   addListItem(newTask);
+  tasks.push(newTask);
+  input.value = ''; // empty after adding 1 task
 });
 
-function addListItem(task:taskType){
-
+function addListItem(task: taskType) {
+  let li = document.createElement('li');
+  let label = document.createElement('label');
+  let checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.checked = task.completed;
+  checkBox.addEventListener('change', () => {
+    task.completed = checkBox.checked;
+  });
+  label.append(checkBox, task.title);
+  li.append(label);
+  list?.append(li);
 }
 
 export {};
