937cea250bce2be52f42d650f3bf6c041d12a7fd
jafar <jafar.fahimi12@gmail.com>
2022-11-16 10:39:55 +0430

    installed @types/uuid

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
-import {v4 as uuidV4} from 'uuid';
-console.log('hi');
+import { v4 as uuidV4 } from 'uuid';
+type taskType = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
-console.log(uuidV4());
+const list = document.querySelector<HTMLUListElement>('list'); // .getElementById don't has this generic func <>
+const form = document.getElementById('new-task-form') as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>('new-task-title');
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input?.value == '' || input?.value == null) return;
+  // input is no longer null from now on.
+  const newTask : taskType = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  addListItem(newTask);
+});
+
+function addListItem(task:taskType){
+
+}
+
+export {};
